c235fa20cd3726c2c957b40ca54ab3d917d039c8
Tassawwur Hussain Joiya <joiyaseditz@gmail.com>
2024-12-13 11:56:57 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default {
-  base: '/MotionDrivenUI/',  // Replace with your repo name
+  base: '/MotionDrivenUI/', 
 };
+
